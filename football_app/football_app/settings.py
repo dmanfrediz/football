1341e7568c7535b577798dede5f072f794d27a2c
Ubuntu <ubuntu@ip-172-31-24-44.us-east-2.compute.internal>
2022-07-03 18:05:51 +0000

    added branch for AWS deploy version

--- football_app/football_app/settings.py
+++ football_app/football_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3dluvi@$s7$0e8*5e5(rtauhs4birit&&aqt*k65%0+eo5jb&*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['football-env.eba-th3ywaab.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'football_app.wsgi.application'
+WSGI_APPLICATION = 'football_app/wsgi.py'
 
 
 # Database
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
