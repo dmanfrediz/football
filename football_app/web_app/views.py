7d479c4e8ead4dc63cd39e9482818daba15ca30d
Daniel Manfrediz <daniel@Daniels-Mac-mini.local>
2022-07-12 16:35:42 -0500

    formatting changes and git ignore test

--- football_app/web_app/views.py
+++ football_app/web_app/views.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 import json
 import datetime as dt
+import os
 import getpass as gt
-from .models import Post
 
 # Create your views here.
 
@@ -14,9 +14,22 @@ from .models import Post
 #     return render(request, 'hello_football.html', context={'name': '{}'.format(user), 'title': 'Football App',
 #                                                            'posts': Post.objects.all()})
 
+config = open(os.path.dirname(os.path.realpath(__file__)) + '/test.config', "r")
+
+with config as f:
+    lines = f.readlines()
+    columns = lines[0].strip()
+    print(f"Columns={columns}")
+
+#
+# with open("secrets.txt") as f:
+#     lines = f.readlines()
+#     username = lines[0].strip()
+#     password = lines[1].strip()
+#     print(f"USERNAME={username}, PASSWORD={password}")
 
 def table(request):
-    df = pd.DataFrame(np.random.randint(0, 100, size=(15, 4)), columns=list('ABCD'))
+    df = pd.DataFrame(np.random.randint(0, 100, size=(15, 4)), columns=list(f'{columns}'))
     json_records = df.reset_index().to_json(orient='records')
     data = json.loads(json_records)
     context = {'d': data}
